540c56a3606368685a941d03ef055e6709439b2f
João de Amorim <johnamorim@outlook.com>
2024-02-26 19:21:15 -0300

    Formatação de Inputs, Definição de Tipo de Cliente e Função de Alerta.

---
+++ Logistics.Dynamics365/WebResources/Logistics.Dynamics365.Utilidades.js
@@ -0,0 +1,29 @@
+﻿if (typeof (Logistics) === "undefined") Logistics = {};
+if (typeof (Logistics.Utilidades) === "undefined") Logistics.Utilidades = {};
+
+
+Logistics.Utilidades = {
+
+    Alerta: function (titulo, texto) {
+
+        let conteudo = {
+
+            confirmButtonLabel: "OK",
+            title: titulo,
+            text: texto
+
+        }
+
+        let dialogConfig = {
+
+            height: 120,
+            width: 200
+
+        }
+
+        Xrm.Navigation.openAlertDialog(conteudo, dialogConfig)
+
+
+
+    }
+}
